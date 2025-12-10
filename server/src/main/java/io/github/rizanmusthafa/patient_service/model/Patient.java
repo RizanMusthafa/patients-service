b9bef502ee4369906e92cc1c08761afde69d9ab8
Rizan Mohomed <musthafarizan@gmail.com>
2025-12-10 13:08:03 +0530

    patient Entity and Service implementations

---
+++ server/src/main/java/io/github/rizanmusthafa/patient_service/model/Patient.java
@@ -0,0 +1,63 @@
+package io.github.rizanmusthafa.patient_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "patients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Column(name = "address")
+  private String address;
+
+  @Column(name = "city")
+  private String city;
+
+  @Column(name = "state")
+  private String state;
+
+  @Column(name = "zip_code")
+  private String zipCode;
+
+  @Column(name = "phone_number")
+  private String phoneNumber;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
+  @PrePersist
+  protected void onCreate() {
+    createdAt = LocalDateTime.now();
+    updatedAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  protected void onUpdate() {
+    updatedAt = LocalDateTime.now();
+  }
+}
